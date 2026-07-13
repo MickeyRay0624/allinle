97dc1c0c85854128ba649b9efaf3066780a6f5eb
MickeyRay0624 <1692067590@qq.com>
2026-07-13 14:16:12 +0700

    fix: use production websocket in devtools

--- apps/miniprogram/utils/config.js
+++ apps/miniprogram/utils/config.js
@@ -2,23 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 function getDefaultApiBase() {
-    try {
-        const sys = wx.getSystemInfoSync();
-        if (sys.platform === "devtools") {
-            return "http://127.0.0.1:3000/api";
-        }
-    }
-    catch (_) { }
     return "https://api.poker.lmqstudio.com/api";
 }
 function getDefaultWsBase() {
-    try {
-        const sys = wx.getSystemInfoSync();
-        if (sys.platform === "devtools") {
-            return "http://127.0.0.1:3000/practice-room";
-        }
-    }
-    catch (_) { }
     return "https://api.poker.lmqstudio.com/practice-room";
 }
 exports.config = {
--- apps/miniprogram/utils/config.ts
+++ apps/miniprogram/utils/config.ts
@@ -1,20 +1,8 @@
 function getDefaultApiBase(): string {
-  try {
-    const sys = wx.getSystemInfoSync();
-    if (sys.platform === "devtools") {
-      return "http://127.0.0.1:3000/api";
-    }
-  } catch (_) {}
   return "https://api.poker.lmqstudio.com/api";
 }
 
 function getDefaultWsBase(): string {
-  try {
-    const sys = wx.getSystemInfoSync();
-    if (sys.platform === "devtools") {
-      return "http://127.0.0.1:3000/practice-room";
-    }
-  } catch (_) {}
   return "https://api.poker.lmqstudio.com/practice-room";
 }
 
